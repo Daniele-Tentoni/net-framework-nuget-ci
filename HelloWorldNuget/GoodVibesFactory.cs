5ef87182cdd96b121cabcaffa358aa5a785f7d54
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-11-25 12:28:10 +0100

    Add get many vibes method.

--- HelloWorldNuget/GoodVibesFactory.cs
+++ HelloWorldNuget/GoodVibesFactory.cs
@@ -1,7 +1,16 @@
 ﻿namespace HelloWorldNuget
 {
+    using System.Collections.Generic;
+
     public static class GoodVibesFactory
     {
         public static string GetAGoodVibe() => "Hello World!";
+        public static IEnumerable<string> GetManyGoodVibes(int number = 1)
+        {
+            var list = new List<string>(number);
+            for (int i = 0; i < number; i++)
+                list.Add(GetAGoodVibe());
+            return list;
+        }
     }
 }
