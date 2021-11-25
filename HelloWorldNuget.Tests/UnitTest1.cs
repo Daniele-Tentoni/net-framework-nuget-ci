98ade5512aa41a6ec121f1231837cfb652519373
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-11-25 14:08:53 +0100

    Add unit tests.

---
+++ HelloWorldNuget.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace HelloWorldNuget.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
