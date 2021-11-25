648be58c875e74590537dcfa102ef275f47dc4db
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-11-25 12:14:23 +0100

    Created .Net Core 3.1 application.

---
+++ HelloWorldCore/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorldCore
+{
+    using HelloWorldNuget;
+
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vibe = GoodVibesFactory.GetAGoodVibe();
+            Console.WriteLine("Hello World {0}!", vibe);
+            Console.Read();
+        }
+    }
+}
