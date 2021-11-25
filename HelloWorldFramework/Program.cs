7b2e918c814605023fec8818e0c3fcce6010f5eb
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-11-25 12:10:25 +0100

    .Net Framework application.

---
+++ HelloWorldFramework/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorldFramework
+{
+    using HelloWorldNuget;
+
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vibes = GoodVibesFactory.GetAGoodVibe();
+            Console.WriteLine("Vibes:", vibes);
+            Console.Read();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Hello World Nuget
+
+First use of Github Actions to deploy a Nuget package that use .Net Standard 2.0 library.
+
+## How to use
+
+Each push will trigger a deploy to Nuget.
+
+Import .Net Standard package to .Net Framework 4.6.1 projects.
